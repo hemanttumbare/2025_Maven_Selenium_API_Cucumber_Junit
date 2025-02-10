7c5add5f81409e3fde069939fdb3f4c21051ad4a
Hemant Tumbare <hemant_tumbare@hotmail.com>
2025-02-10 08:06:32 +0530

    Updated allure jvm version

--- src/test/java/com/cucumber/runner/BddTest.java
+++ src/test/java/com/cucumber/runner/BddTest.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources",
                 glue = {"com.cucumber.stepDefinitions","com.cucumber.hooks"},
-                plugin = {"pretty","io.qameta.allure.cucumber4jvm.AllureCucumber4Jvm"},
+                plugin = {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
                 monochrome = true)
 public class BddTest {
 
